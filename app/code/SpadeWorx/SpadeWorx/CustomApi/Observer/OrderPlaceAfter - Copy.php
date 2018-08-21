e263703425e4be7b5488f3283a001cecdaf7286e
Jivaji Ihare <jivaji.ihare@asiapac.local>
2018-08-21 16:59:20 +0530

    test commit

---
+++ app/code/SpadeWorx/SpadeWorx/CustomApi/Observer/OrderPlaceAfter - Copy.php
@@ -0,0 +1,72 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace SpadeWorx\CustomApi\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class OrderPlaceAfter implements ObserverInterface
+{
+    /**
+     * Order Model
+     *
+     * @var \Magento\Sales\Model\Order $order
+     */
+    protected $order;
+
+     public function __construct(
+        \Magento\Sales\Model\Order $order
+    )
+    {
+        
+        $this->order = $order;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        
+        $orderId = $observer->getEvent()->getOrderIds();
+        $orderObj = $this->order->load($orderId);
+
+        //get Order All Item
+        //$itemCollection = $orderObj->getItemsCollection();
+        //$customer = $orderObj->getCustomerId(); // using this id you can get customer name
+		
+		$orderData = array();
+		$orderdata['order_id'] = $orderObj->getEntityId();
+		$orderdata['customer_id']  =  $orderObj->getCustomerId();
+		$orderdata['customer_email'] = $orderObj->getCustomerEmail();
+		$orderdata['customer_firstname'] = $orderObj->getCustomerFirstname();
+		$orderdata['customer_lastname'] = $orderObj->getCustomerLastname();
+		$orderdata['status'] = $orderObj->getStatus();
+		$orderdata['total_qty_ordered'] = $orderObj->getTotalQtyOrdered();
+		$orderdata['grand_total'] = $orderObj->getGrandTotal();
+
+  /*
+		//$url = 'http://localhost/magento22/testapi.php';
+		$url = 'http://104.211.89.57:8080/api/User/UserAuthentication?userInfo=dXNlck5hbWU9c2FyYW5nLmt1bGthcm5pQHNwYWRld29yeC5jb20mUGFzc3dvcmQ9c3BhZGVAMTIz';
+		$fields_string = json_encode($orderdata);
+		$fields = sizeof($orderData);
+
+
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL, $url);
+		//curl_setopt($ch,CURLOPT_POST, count($fields));
+		//curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE); 
+
+		$result = curl_exec($ch);
+		if(!$result){
+			print_r("Connection Failure"); die;
+		}else{
+			echo "<result>";print_r($result); echo "</br>";
+		}
+		curl_close($ch);
+		*/
+    }
+}
---
+++ app/etc/env.php
@@ -0,0 +1,70 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'db' => 
+  array (
+    'connection' => 
+    array (
+      'indexer' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'volume',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+        'persistent' => NULL,
+      ),
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'volume',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => 'vol_',
+  ),
+  'crypt' => 
+  array (
+    'key' => '05653c273eb4cb60a92c2517fe20906a',
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'target_rule' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 10 Apr 2018 10:35:57 +0000',
+  ),
+);
